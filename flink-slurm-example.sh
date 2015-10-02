2f9d7fece31ea3eb73652c0f51fb95af06b753a9
Robert Schmidtke <ro.schmidtke@gmail.com>
2015-10-02 10:28:33 +0200

    proper usage; separate conf directory for each slurm job

--- flink-slurm-example.sh
+++ flink-slurm-example.sh
@@ -20,47 +20,54 @@
 #SBATCH --job-name flink-slurm
 #SBATCH --nodes=8
 
+USAGE="Usage: sbatch -p<PARTITION> -A<ACCOUNT> flink-slurm-example.sh"
+
+if [[ -z $SLURM_JOB_ID ]]; then
+    echo "No Slurm environment detected. $USAGE"
+    exit 1
+fi
+
 export FLINK_HOME="$HOME/flink/build-target"
 
-# First node is master, all others are slaves
+# Custom conf directory for this job
+export FLINK_CONF_DIR="${FLINK_HOME}"/conf-slurm-$SLURM_JOB_ID
+cp -R "${FLINK_HOME}"/conf "${FLINK_CONF_DIR}"
+
+# First Slurm node is master, all others are slaves
 FLINK_NODES=(`scontrol show hostnames`)
 FLINK_MASTER=${FLINK_NODES[0]}
 FLINK_SLAVES=(${FLINK_NODES[@]:1})
 
-# backup
-mv "${FLINK_HOME}"/conf/slaves "${FLINK_HOME}"/conf/slaves.orig
-cp "${FLINK_HOME}"/conf/flink-conf.yaml "${FLINK_HOME}"/conf/flink-conf.yaml.orig
-
-printf "%s\n" "${FLINK_SLAVES[@]}" > "${FLINK_HOME}/conf/slaves"
+printf "%s\n" "${FLINK_SLAVES[@]}" > "${FLINK_CONF_DIR}/slaves"
 
 ### Inspect nodes for CPU and memory and configure Flink accordingly ###
 
 echo
 echo "-----BEGIN FLINK CONFIG-----"
 
-sed -i "/jobmanager\.rpc\.address/c\jobmanager.rpc.address: $FLINK_MASTER" $FLINK_HOME/conf/flink-conf.yaml
+sed -i "/jobmanager\.rpc\.address/c\jobmanager.rpc.address: $FLINK_MASTER" "${FLINK_CONF_DIR}"/flink-conf.yaml
 echo "jobmanager.rpc.address: $FLINK_MASTER"
 
 # 40 percent of available memory
 JOBMANAGER_HEAP=$(srun --nodes=1-1 --nodelist=$FLINK_MASTER awk '/MemTotal/ {printf( "%.2d\n", ($2 / 1024) * 0.4 )}' /proc/meminfo)
-sed -i "/jobmanager\.heap\.mb/c\jobmanager.heap.mb: $JOBMANAGER_HEAP" $FLINK_HOME/conf/flink-conf.yaml
+sed -i "/jobmanager\.heap\.mb/c\jobmanager.heap.mb: $JOBMANAGER_HEAP" "${FLINK_CONF_DIR}"/flink-conf.yaml
 echo "jobmanager.heab.mb: $JOBMANAGER_HEAP"
 
 # 80 percent of available memory
 TASKMANAGER_HEAP=$(srun --nodes=1-1 --nodelist=$FLINK_MASTER awk '/MemTotal/ {printf( "%.2d\n", ($2 / 1024) * 0.8 )}' /proc/meminfo)
-sed -i "/taskmanager\.heap\.mb/c\taskmanager.heap.mb: $TASKMANAGER_HEAP" $FLINK_HOME/conf/flink-conf.yaml
+sed -i "/taskmanager\.heap\.mb/c\taskmanager.heap.mb: $TASKMANAGER_HEAP" "${FLINK_CONF_DIR}"/flink-conf.yaml
 echo "taskmanager.heap.mb: $TASKMANAGER_HEAP"
 
 # number of phyical cores per task manager
 NUM_CORES=$(srun --nodes=1-1 --nodelist=$FLINK_MASTER cat /proc/cpuinfo | egrep "core id|physical id" | tr -d "\n" | sed s/physical/\\nphysical/g | grep -v ^$ | sort | uniq | wc -l)
-sed -i "/taskmanager\.numberOfTaskSlots/c\taskmanager.numberOfTaskSlots: $NUM_CORES" $FLINK_HOME/conf/flink-conf.yaml
+sed -i "/taskmanager\.numberOfTaskSlots/c\taskmanager.numberOfTaskSlots: $NUM_CORES" "${FLINK_CONF_DIR}"/flink-conf.yaml
 echo "taskmanager.numberOfTaskSlots: $NUM_CORES"
 
 # number of nodes * number of physical cores
 # PARALLELISM=$(cat $FLINK_HOME/conf/slaves | wc -l)
 # PARALLELISM=$((PARALLELISM * NUM_CORES))
 PARALLELISM=1
-sed -i "/parallelism\.default/c\parallelism.default: $PARALLELISM" $FLINK_HOME/conf/flink-conf.yaml
+sed -i "/parallelism\.default/c\parallelism.default: $PARALLELISM" "${FLINK_CONF_DIR}"/flink-conf.yaml
 echo "parallelism.default: $PARALLELISM"
 
 echo "-----END FLINK CONFIG---"
@@ -74,9 +81,5 @@ sleep 60
 
 srun --nodes=1-1 --nodelist=${FLINK_MASTER} "${FLINK_HOME}"/bin/stop-slurm.sh
 sleep 60
-
-# restore
-mv "${FLINK_HOME}"/conf/flink-conf.yaml.orig "${FLINK_HOME}"/conf/flink-conf.yaml
-mv "${FLINK_HOME}"/conf/slaves.orig "${FLINK_HOME}"/conf/slaves
 
 echo "Done."
